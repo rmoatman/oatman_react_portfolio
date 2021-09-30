2348f506b1b32e8936daee3b4b462e9f161bb410
RMOATMAN <raemarie.oatman@gmail.com>
2021-09-30 16:10:16 -0400

    added About Me

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -1,10 +1,39 @@
 import React from 'react';
+import Hero from '../component/Hero';
+import Content from '../component/Content';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import oatman from '../assets/images/oatman_photo.jpg';
 
 function AboutPage(props) {
     
     return(
-        <p>About Me Page Works!</p>
+        <div>
+            <Hero title={props.title}/>
+
+            <Content>
+
+                <Row className="d-flex justify-content-center" >
+
+                    <Col>
+                        <div className="fs-2" md={6}>
+                            <p><span>Experienced Leader and Group Facilitator with MBA interested in joining your team as an entry-level Web Developer. Successfully completed Ohio State University Full-Stack Developer Bootcamp. For-profit, not-for-profit, and higher-education work experience. Excellent communication and team skills.</span><br></br><br></br></p>
+                            
+                            <p className="d-flex justify-content-center">Detailed * Curious * Organized * Creative * Passionate</p><br></br>
+                            
+                            <p className="d-flex justify-content-center">JavaScript * HTML * CSS * JQuery * API * React.js * MySQL * Node.js</p>
+                        </div>
+                    </Col>
+
+                    <Col md={3}>
+                        <img src={oatman} height="400px" alt="Professional headshot of Raemarie Oatman"></img>
+                    </Col>
+
+                </Row>
+            </Content>
+        </div>
     );
+    
 }
 
 export default AboutPage;
